b08649bc1384e4eb0d6f420f2e1d5dce8a86791c
artyom Titanov <artitanov@gmail.com>
2019-03-04 10:51:43 +0300

    feat: ready task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let sumUPLovers = 0;
+  let sumUPfinish = 0;
+  
+  for (i=0; preferences.length>i; i++){
+    
+  number1 = preferences[i];
+  number2 = preferences[number1-1];
+  number3 = preferences[number2-1];
+  
+  if (number3 == i+1) {sumUPLovers++;};
+  if (sumUPLovers === 3) {sumUPfinish++; sumUPLovers = 0;};
+  };
+
+  return sumUPfinish;
 };
